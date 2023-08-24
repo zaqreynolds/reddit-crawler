15c37f73379554976a6045d5fc72a48bd7b5ae2a
Zaq Reynolds <zaqreynolds@gmail.com>
2023-08-24 16:54:17 -0700

    prep

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,16 @@ import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div id="generalBody">
+    <div
+      id="generalBody"
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        flexDirection: "column",
+        // overflow: "hidden",
+      }}
+    >
       <div id="header">
         <Link to="/">
           <h1>Reddit Crawler</h1>
