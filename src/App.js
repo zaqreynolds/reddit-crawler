60b80a62ad68befebe12bbf4a69baab992d9b8f7
Zaq Reynolds <zaqreynolds@gmail.com>
2023-08-25 17:29:42 -0700

    feat: more prep

--- src/App.js
+++ src/App.js
@@ -3,15 +3,36 @@ import { Results } from "./components/Results";
 import { Route, Routes } from "react-router-dom";
 import { Details } from "./components/Details";
 import Layout from "./components/Layout";
+import {
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+  useMediaQuery,
+} from "@mui/material";
+import Green from "@mui/material/colors/green";
 
 function App() {
+  const isSmallViewport = useMediaQuery("(max-width:600px)");
+  const theme = createTheme({
+    typography: {
+      fontSize: isSmallViewport ? 12 : 14,
+    },
+    palette: {
+      primary: Green,
+    },
+  });
+
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Results />} />
-        <Route path=":id" element={<Details />} />
-      </Route>
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Results />} />
+            <Route path=":id" element={<Details />} />
+          </Route>
+        </Routes>
+      </CssBaseline>
+    </ThemeProvider>
   );
 }
 
